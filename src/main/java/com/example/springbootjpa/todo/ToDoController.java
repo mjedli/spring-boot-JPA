d8a36a55bad4e92dc0da59000717cef83e3ad924
mjedli <jedli.mejdi.recup@gmail.com>
2020-06-27 17:24:18 +0200

    spring boot jpa

--- src/main/java/com/example/springbootjpa/todo/ToDoController.java
+++ src/main/java/com/example/springbootjpa/todo/ToDoController.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,6 +29,13 @@ public class ToDoController {
 		todo.setDescription("description");
 		data.add(todo);
 		return data;
+	}
+
+	
+	@PostMapping(value= {"/", ""})
+	private ToDo saveToDo(@RequestBody ToDo todo) {
+		data.add(todo);
+		return todo;
 	}
 	
 }
